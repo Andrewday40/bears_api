2bd1e02c0beb4a7552986e257b88e54c2653e541
Andrew <andrewday40@gmail.com>
2016-07-05 20:26:09 -0400

    basic routes added

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ var server = express();
 var port = process.env.port || 8080;
 
 server.get('/bears', function(request, response){
-
+  response.send(bears);
 });
 
+server.get('/bears/:id', function(request, response){
+  response.send(bear);
+});
+
+server.post('/bears/:id', function(request, response){
+  response.send(result);
+});
+
+server.put('/bears/:id', function(request, response){
+  response.send(updatedbear);
+});
+
+server.delete('/bears/:id', function(request, response){
+  response.send(bear)
+})
 
 server.listen(port, function(){
   console.log('Now listening to port:..', port);
